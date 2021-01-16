a226d191b1bb1d4e88a3b0f98e3642a9c1828d62
alexrus79 <alexrus79@yandex.ru>
2021-01-16 15:39:37 +0900

    Update Program.cs

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -6,7 +6,21 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool boolFlag;
+            byte firstNum = 255;
+            sbyte secondNum = 127;
+            string firstStr = "firstNum";
+            string secondStr = "secondNum";
+            string symbolStr  = "!?<>= ";             
+            
+            boolFlag = firstNum > secondNum; //true
+            Console.WriteLine(firstStr + symbolStr[5] + symbolStr[4] + symbolStr[5] + firstNum);
+            Console.WriteLine(secondStr + symbolStr[5] + symbolStr[4] + symbolStr[5] + secondNum);
+            Console.WriteLine(firstStr + symbolStr[5] + symbolStr[3] + symbolStr[5] + secondStr + 
+                symbolStr[1] + symbolStr[5]+ boolFlag + symbolStr[0]);
+            boolFlag = firstNum < secondNum; //false
+            Console.WriteLine(firstStr + symbolStr[5] + symbolStr[2] + symbolStr[5] + secondStr + 
+                symbolStr[1] + symbolStr[5] + boolFlag + symbolStr[0]);
         }
     }
 }
