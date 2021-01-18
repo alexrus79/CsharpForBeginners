033a950c75e50a9c4b6f9451fba2a854dee96f54
alexrus79 <alexrus79@yandex.ru>
2021-01-19 00:53:47 +0900

    Using methods

--- MyFirstConsoleApp/HomeWorkTwo/Program.cs
+++ MyFirstConsoleApp/HomeWorkTwo/Program.cs
@@ -12,7 +12,7 @@ namespace RussianBlackJack
             string questionMore = "Дилер: Еще? (y/n)";
             string phraseDealerPas = "Дилер: Я пас!";
             string phraseDealerHand = "У дилера на руках: ";
-            string phraseYourHand = "У Вас в руке: ";
+            string phrasePlayerHand = "У Вас в руке: ";
             string phraseYouLose = "Вы проиграли!";
             string phraseYouWin = "Вы выиграли!!!";
             bool againGameAttribute = false;
@@ -41,203 +41,143 @@ namespace RussianBlackJack
                     Random random = new Random();
                     int[] dealerHand = new int[1];
                     int[] playerHand = new int[1];
-                    int[] newDeck = new int[deck.Length - 1]; //создаем массив для временной колоды
-
-                    //сдаем карту игроку
-
-                    int shuffle = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
-                    playerHand[playerHand.Length - 1] = deck[shuffle]; //присваиваем карту игроку                                                          
-                    for (int i = 0; i < shuffle; i++) //удаляем из колоды вытянутую карту
-                    {
-                        newDeck[i] = deck[i];
-                    }
-                    for (int i = shuffle; i < deck.Length - 1; i++)
-                    {
-                        newDeck[i] = deck[i + 1];
-                    }
-                    deck = newDeck; //колода стала на одну карту меньше
-                    Array.Resize(ref newDeck, deck.Length - 1);
-                    Array.Clear(newDeck, 0, newDeck.Length); //очищаем массив
-
-                    //компьютер берет себе карту
-
-                    shuffle = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
-                    dealerHand[dealerHand.Length - 1] = deck[shuffle]; //присваиваем карту дилеру                             
-                    for (int i = 0; i < shuffle; i++) // удаляем из колоды вытянутую карту
-                    {
-                        newDeck[i] = deck[i];
-                    }
-                    for (int i = shuffle; i < deck.Length - 1; i++)
-                    {
-                        newDeck[i] = deck[i + 1];
-                    }
-                    deck = newDeck; //колода стала на одну карту меньше
-                    Array.Resize(ref newDeck, deck.Length - 1); //укорачиваем массив на один элемент
-                    Array.Clear(newDeck, 0, newDeck.Length); //очищаем массив
-
-                    Console.Write(phraseYourHand); //выводим руку игрока на экран
-                    for (int i = 0; i < playerHand.Length; i++)
-                    {
-                        Console.Write(playerHand[i] + " ");
-                    }
-                    Console.WriteLine();
-
                     bool dealerPas = false;
                     bool playerPas = false;
+                    bool winHand = false;
+                    bool playerWinHand = false;
+                    bool dealerWinHand = false;
                     int summDealerHand;
                     int summPlayerHand;
+
+                    int card = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
+                    
+                    playerHand[playerHand.Length - 1] = deck[card]; //присваиваем карту игроку                                                          
+
+                    deck = RemoveCardFromDeck(deck, card);
+
+                    card = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
+                    
+                    dealerHand[dealerHand.Length - 1] = deck[card]; //присваиваем карту дилеру                             
+
+                    deck = RemoveCardFromDeck(deck, card);
+                    
+                    PrintHand(phrasePlayerHand, playerHand);
+                    Console.WriteLine();
 
                     do
                     {
                         do
-                        {                        
+                        {
                             Console.Write(questionMore);
                             answer = Console.ReadLine();
                         } while (answer != "y" && answer != "n");
-                        if (answer == "y")
-                        {
+                        
+                        if (answer == "y" && playerHand.Length < 20)
+                        {                            
                             //сдаем карту игроку
                             Array.Resize(ref playerHand, playerHand.Length + 1); //увеличиваем массив на 1 элемент
-                            shuffle = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
-                            playerHand[playerHand.Length - 1] = deck[shuffle]; //присваиваем карту игроку                                                          
-                            for (int i = 0; i < shuffle; i++) //удаляем из колоды вытянутую карту
-                            {
-                                newDeck[i] = deck[i];
-                            }
-                            for (int i = shuffle; i < deck.Length - 1; i++)
-                            {
-                                newDeck[i] = deck[i + 1];
-                            }
-                            deck = newDeck; //колода стала на одну карту меньше
-                            Array.Resize(ref newDeck, deck.Length - 1); //укорачиваем массив на один элемент
-                            Array.Clear(newDeck, 0, newDeck.Length); //очищаем массив
+                            
+                            card = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
+                            
+                            playerHand[playerHand.Length - 1] = deck[card]; //присваиваем карту игроку                                                          
 
-                            summDealerHand = 0;
-                            foreach (int item in dealerHand)
-                            {
-                                if (item == 11 && (summDealerHand+11) > 21) //Если с тузом перебор то туз равен 1
-                                {
-                                    summDealerHand = summDealerHand + 1;
-                                }
-                                else summDealerHand = summDealerHand + item;
-                            }
+                            deck = RemoveCardFromDeck(deck, card);
+
+                            summDealerHand = GetSumHand(dealerHand, out winHand);
 
                             if (summDealerHand < 16)
                             {
                                 //компьютер берет себе карту
 
                                 Array.Resize(ref dealerHand, dealerHand.Length + 1); //увеличиваем массив на 1 элемент
-                                shuffle = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
-                                dealerHand[dealerHand.Length - 1] = deck[shuffle]; //присваиваем карту дилеру                             
-                                for (int i = 0; i < shuffle; i++) // удаляем из колоды вытянутую карту
-                                {
-                                    newDeck[i] = deck[i];
-                                }
-                                for (int i = shuffle; i < deck.Length - 1; i++)
-                                {
-                                    newDeck[i] = deck[i + 1];
-                                }
-                                deck = newDeck; //колода стала на одну карту меньше
-                                Array.Resize(ref newDeck, deck.Length - 1); //укорачиваем массив на один элемент
-                                Array.Clear(newDeck, 0, newDeck.Length); //очищаем массив
+                                
+                                card = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
+                                
+                                dealerHand[dealerHand.Length - 1] = deck[card]; //присваиваем карту дилеру                             
+
+                                deck = RemoveCardFromDeck(deck, card);
                             }
                             else
                             {
-
                                 dealerPas = true;
                                 Console.WriteLine(phraseDealerPas);
                             }
-
-
-                            Console.Write(phraseYourHand); //выводим руку игрока на экран
-                            for (int i = 0; i < playerHand.Length; i++)
-                            {
-                                Console.Write(playerHand[i] + " ");
-                            }
+                            PrintHand(phrasePlayerHand, playerHand);
                             Console.WriteLine();
                         }
                         else
                         {
                             playerPas = true;
-                            summDealerHand = 0;
-                            foreach (int item in dealerHand)
-                            {
-                                if (item == 11 && (summDealerHand + 11) > 21) //Если с тузом перебор то туз равен 1
-                                {
-                                    summDealerHand = summDealerHand + 1;
-                                }
-                                else summDealerHand = summDealerHand + item;
-                            }
+
+                            summDealerHand = GetSumHand(dealerHand, out winHand);
+
                             while (summDealerHand < 16)
                             {
                                 //компьютер берет себе карту
 
                                 Array.Resize(ref dealerHand, dealerHand.Length + 1); //увеличиваем массив на 1 элемент
-                                shuffle = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
-                                dealerHand[dealerHand.Length - 1] = deck[shuffle]; //присваиваем карту дилеру                             
-                                for (int i = 0; i < shuffle; i++) // удаляем из колоды вытянутую карту
-                                {
-                                    newDeck[i] = deck[i];
-                                }
-                                for (int i = shuffle; i < deck.Length - 1; i++)
-                                {
-                                    newDeck[i] = deck[i + 1];
-                                }
-                                deck = newDeck; //колода стала на одну карту меньше
-                                Array.Resize(ref newDeck, deck.Length - 1); //укорачиваем массив на один элемент
-                                Array.Clear(newDeck, 0, newDeck.Length); //очищаем массив
-                                summDealerHand = 0;
-                                foreach (int item in dealerHand)
-                                {
-                                    if (item == 11 && (summDealerHand + 11) > 21) //Если с тузом перебор то туз равен 1
-                                    {
-                                        summDealerHand = summDealerHand + 1;
-                                    }
-                                    else summDealerHand = summDealerHand + item;
-                                }
+                                
+                                card = random.Next(0, deck.Length); //вытаскиваем из колоды случайную карту
+                                
+                                dealerHand[dealerHand.Length - 1] = deck[card]; //присваиваем карту дилеру                             
 
+                                deck = RemoveCardFromDeck(deck, card);
+
+                                summDealerHand = GetSumHand(dealerHand, out winHand);
                             }
                             dealerPas = true;
                         }
                     } while (playerPas == false || dealerPas == false);
 
-                    summDealerHand = 0;
-                    summPlayerHand = 0;
+                    PrintHand(phrasePlayerHand, playerHand);
 
-                    Console.Write(phraseYourHand); //выводим руку игрока на экран
-                    for (int i = 0; i < playerHand.Length; i++)
+                    summPlayerHand = GetSumHand(playerHand, out winHand);
+
+                    if (winHand == true)
                     {
-                        if (playerHand[i] == 11 && (summPlayerHand + playerHand[i]) > 21) //Если с тузом перебор то туз равен 1
-                        {
-                            summPlayerHand = summPlayerHand + 1;
-                        }
-                        else summPlayerHand = summPlayerHand + playerHand[i];
-                        Console.Write(playerHand[i] + " ");
+                        playerWinHand = true;
+                        winHand = false;
                     }
+
                     Console.Write("(" + summPlayerHand + ")");
                     Console.WriteLine();
 
-                    Console.Write(phraseDealerHand); //выводим руку дилера на экран
-                    for (int i = 0; i < dealerHand.Length; i++)
+                    PrintHand(phraseDealerHand, dealerHand);
+
+                    summDealerHand = GetSumHand(dealerHand, out winHand);
+
+                    if (winHand == true)
                     {
-                        if (dealerHand[i] == 11 && (summDealerHand + dealerHand[i]) > 21) //Если с тузом перебор то туз равен 1
-                        {
-                            summDealerHand = summDealerHand + 1;
-                        }
-                        else summDealerHand = summDealerHand + dealerHand[i];
-                        Console.Write(dealerHand[i] + " ");
+                        dealerWinHand = true;
+                        winHand = false;
                     }
+
                     Console.Write("(" + summDealerHand + ")");
                     Console.WriteLine();
-
-                    if (summDealerHand > 21 || summPlayerHand == 21) summDealerHand = 0;
-                    if (summPlayerHand > 21) summPlayerHand = 0;
-                    if (summDealerHand > summPlayerHand || summDealerHand == summPlayerHand)
+                    if (playerWinHand == true && dealerWinHand == false)
                     {
-                        summPlayerHand = 0;
+                        //Если у игрока два туза или пять картинок, то исключить все проверки
                     }
-
-                    if (summDealerHand >= summPlayerHand)
+                    else if (dealerWinHand == true && playerWinHand == false)
+                    {
+                        //Если у дилера два туза или пять картинок, то исключить все проверки
+                    }
+                    else if (dealerWinHand == true && playerWinHand == true)
+                    {
+                        playerWinHand = false; //Если у обоих игроков по туза или по пять картинок, то выиграл дилер
+                    }
+                    else if (summDealerHand > 21 || summPlayerHand == 21)
+                    {
+                        playerWinHand = true;
+                    }
+                    else if (summPlayerHand > 21 || 
+                        summDealerHand > summPlayerHand || 
+                        summDealerHand == summPlayerHand)
+                    {
+                        dealerWinHand = true;
+                    } 
+                    
+                    if (dealerWinHand == true && playerWinHand == false)
                     {
                         Console.WriteLine(phraseYouLose);
                     }
@@ -245,12 +185,13 @@ namespace RussianBlackJack
                     {
                         Console.WriteLine(phraseYouWin);
                     }
+
                     do
                     {
                         Console.Write(questionPlayAgain);
                         answer = Console.ReadLine();
                     } while (answer != "y" && answer != "n");
-                    
+
                     if (answer == "y")
                     {
                         againGameAttribute = true;
@@ -260,6 +201,64 @@ namespace RussianBlackJack
                 }
                 else againGameAttribute = false;
             } while (againGameAttribute);
+        }
+
+        private static void PrintHand(string phraseYourHand, int[] playerHand)
+        {
+            Console.Write(phraseYourHand);
+            for (int i = 0; i < playerHand.Length; i++)
+            {
+                Console.Write(playerHand[i] + " ");
+            }
+            
+        }
+
+        private static int[] RemoveCardFromDeck(int[] deck, int index)
+        {
+
+            int[] newDeck = new int[deck.Length - 1];
+            for (int i = 0; i < index; i++)
+            {
+                newDeck[i] = deck[i];
+            }
+            for (int i = index; i < deck.Length - 1; i++)
+            {
+                newDeck[i] = deck[i + 1];
+            }
+            return newDeck;
+        }
+
+        private static int GetSumHand(int[] Hand, out bool winHand)
+        {
+            int sum = 0;
+            bool fiveCardImade = true;
+            winHand = false;
+
+            for (int i = 0; i < Hand.Length; i++)
+            {
+                if (sum == 11 && Hand[i] == 11 && i == 1)
+                {
+                    sum = 21;
+                    winHand = true;
+                    break;
+                } 
+
+                if (Hand[i] == 11 && (sum + 11) > 21) //Если с тузом перебор то туз равен 1
+                {
+                    sum = sum + 1;
+                }
+                else sum = sum + Hand[i];
+                
+                if (Hand[i] > 4) fiveCardImade = false;
+            }
+
+            if (fiveCardImade && Hand.Length == 5)
+            {
+                sum = 21; //усли все картинки и их пять то рука выигрышная
+                winHand = true;
+            }
+            
+            return sum;
         }
     }
 }
---
+++ MyFirstConsoleApp/lessonThree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lessonThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private static void
+        }
+    }
+}
